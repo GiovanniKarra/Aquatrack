e167eca419419b436084c7a3c459293755706449
Gogani <karragiovanni@gmail.com>
2021-11-17 12:22:45 +0100

    fuck

--- simulation.py
+++ simulation.py
@@ -8,6 +8,7 @@ g = 9.81  # accélération de gravitation [m/s**2]
 b = 0.01  # écart des rails [m]
 r = 0.0079375  # rayon de la bille [m]
 h = np.sqrt(r ** 2 - b ** 2 / 4)  # hauteur du centre de la bille sur les rails [m]
+m = 0.016  # masse de la bille
 
 # e1 = 0.001270193299685568  # coefficient de frottement linéaire [m/(m/s)]
 # e1 = 0.000995637359264194
@@ -15,7 +16,6 @@ h = np.sqrt(r ** 2 - b ** 2 / 4)  # hauteur du centre de la bille sur les rails 
 e1 = 0.01
 
 # chemin de la bille (et autres paramètres)
-# xyzPoints = np.loadtxt("points_de_passage.txt", unpack=True)
 xyzPoints = np.loadtxt("points_de_passage.txt", unpack=True)
 sPath, xyzPath, tPath, cPath = p3d.path(xyzPoints)
 
@@ -27,6 +27,7 @@ steps = int(tEnd / dt)  # nombre de pas de la simulation
 tSim = np.zeros(steps + 1)  # temps: array[steps+1] * [s]
 sSim = np.zeros(steps + 1)  # distance curviligne: array[steps+1] * [m]
 VsSim = np.zeros(steps + 1)  # vitesse tangentielle: array[steps+1] * [m/s]
+zSim = np.zeros(steps + 1)
 
 # valeurs initiales:
 tSim[0] = 0
@@ -58,6 +59,23 @@ for i in range(steps):
     xyzMarks[:, i] = xyz
     cMarks[:, i] = Gn
     tMarks[:, i] = gs
+
+    zSim[i] = xyz[2]
+
+M = 1 + 2 / 5 * r ** 2 / h ** 2  # coefficient d'inertie [1]
+
+EpSim = g * zSim  # énergie potentielle spécifique [m**2/s**2]
+EkSim = 0.5 * M * VsSim ** 2  # énergie cinétique spécifique [m**2/s**2]
+
+# plot énergies
+plt.figure()
+plt.plot(tSim, EpSim, 'b-', label='Ep/m')
+plt.plot(tSim, EkSim, 'r-', label='Ek/m')
+plt.plot(tSim, EkSim + EpSim, 'k-', label='E/m')
+plt.legend()
+plt.ylabel('Energy/mass [J/kg]')
+plt.xlabel('t [s]')
+plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
